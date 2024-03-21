8469b85448a29a712b4aa0632a28743da0370593
Scott Jones <scott.jones@sky.uk>
2024-03-21 10:01:24 +0000

    fix goal amount

--- custom_components/monzo/models.py
+++ custom_components/monzo/models.py
@@ -50,7 +50,9 @@ class PotModel(BaseMonzoModel):
         self.name = pot['name']
         self.balance = pot['balance']/100
         self.currency = pot['currency']
-        self.goal_amount = pot.get('goal_amount')/100
+        self.goal_amount = None
+        if ('goal_amount' in pot)
+            self.goal_amount = pot.get('goal_amount')/100
         self.deleted = pot['deleted']
         self.locked = pot['locked']
         self.pot_type = pot['type']
