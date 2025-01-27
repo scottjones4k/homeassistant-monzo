acc75893081c6cd97ba572cb791a109871543ca5
Scott Jones <scott.jones@sky.uk>
2025-01-27 15:34:15 +0000

    fix: update validation

--- custom_components/monzo/api/models/transaction.py
+++ custom_components/monzo/api/models/transaction.py
@@ -2,27 +2,27 @@ from typing import Literal
 from pydantic import BaseModel
 
 class Metadata(BaseModel):
-    notes: str | None
-    pot_id: str | None
-    bills_pot_id: str | None
-    triggered_by: str | None
-    trigger: str | None
-    tokenization_method: str | None
+    notes: str | None = None
+    pot_id: str | None = None
+    bills_pot_id: str | None = None
+    triggered_by: str | None = None
+    trigger: str | None = None
+    tokenization_method: str | None = None
 
 class AtmFeeDetailed(BaseModel):
     withdrawal_amount: int
 
 class Counterparty(BaseModel):
-    name: str | None
-    account_number: str | None
-    sort_code: str | None
+    name: str | None = None
+    account_number: str | None = None
+    sort_code: str | None = None
 
 class Transaction(BaseModel):
     account_id: str
     metadata: Metadata
     scheme: str
-    atm_fee_detailed: AtmFeeDetailed | None
-    counterparty: Counterparty | None
+    atm_fee_detailed: AtmFeeDetailed | None = None
+    counterparty: Counterparty | None = None
     amount: int
     description: str
     currency: str
